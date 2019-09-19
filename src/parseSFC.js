4a7e8f67da52ea760a8bf1f67e2130ded0da4fef
Mactavish <maczhanchao@yahoo.com>
2019-09-19 11:39:18 +0800

    feat: handle different cases in sfc, extract common method, add comments

--- src/parseSFC.js
+++ src/parseSFC.js
@@ -3,17 +3,29 @@ const { parse: parseSFC, compileTemplate } = require('@vue/component-compiler-ut
 const babelParser = require('@babel/parser')
 const babelTraverse = require('@babel/traverse').default
 const babelTypes = require('@babel/types')
-// const codegen = require('@babel/generator').default
 const template = require('@babel/template').default
 const Case = require('case')
 
+/**
+ * @description Parse Vue single file component into asts
+ * @class SFCParser
+ */
 class SFCParser {
+  /**
+   *Creates an instance of SFCParser.
+   * @param {*} {
+   *     source = '', vue sfc source code
+   *     componentName = '', component name for component registration
+   *     babelParserOptions = {} babel parser options
+   *   }
+   * @memberof SFCParser
+   */
   constructor ({
     source = '',
     componentName = '',
-    parserOptions = {}
+    babelParserOptions = {}
   }) {
-    const defaultOptions = {
+    const defaultBabelParserOptions = {
       sourceType: 'unambiguous',
       plugins: [
         'asyncGenerators',
@@ -23,36 +35,82 @@ class SFCParser {
       ]
     }
     this.source = source
+    // force component name in pascal case for simplicity
     this.componentName = Case.pascal(componentName)
     this.extractedImportDeclarations = []
     this.componentOptions = null
     this.componentDeclaration = null
-    this.componentAstFactory = template(`
-      const %%componentName%% = (function() {
-        %%renderFnsAndOtherStatements%%\n
-        const componentOptions = %%componentOptions%%\n
-        return {
-          ...componentOptions,
-          render,
-          staticRenderFns
-        }
-      })();
-    `)
-    this.babelParserOptions = Object.assign({}, defaultOptions, parserOptions)
+    // handle different cases in SFC, but SFC should at least contains a script tag or a template tag
+    this.componentAstFactory = (hasTemplate = true, hasScript = true) => template(`
+    const %%componentName%% = (function() {
+      %%otherStatements%%
+      ${hasTemplate ? '%%renderFns%%\n' : ''}
+      ${hasScript ? 'const componentOptions = %%componentOptions%%\n' : ''}
+      return {
+        ${hasScript ? '...componentOptions,' : ''}
+        ${hasTemplate ? 'render,\nstaticRenderFns' : ''}
+      }
+    })();
+  `)
+    this.babelParserOptions = Object.assign({}, defaultBabelParserOptions, babelParserOptions)
   }
 
+  /**
+   * @description generate component declaration ast for futher usage
+   * @memberof SFCParser
+   */
+  genComponentDeclarationAst () {
+    const hasTemplate = Boolean(this.templateBlock)
+    const hasScript = Boolean(this.scriptBlock)
+    const subsititutions = {
+      componentName: babelTypes.identifier(this.componentName)
+    }
+
+    // when SFC contains a template tag
+    if (hasTemplate) {
+      subsititutions.renderFns = this.templateBlock.ast.program.body.slice()
+      subsititutions.otherStatements = []
+    }
+
+    // when SFC contains a script tag
+    if (hasScript) {
+      subsititutions.componentOptions = this.componentOptions
+      subsititutions.otherStatements = this.scriptBlock.ast.program.body.slice()
+    }
+    this.componentDeclaration = this.componentAstFactory(hasTemplate, hasScript)(subsititutions)
+  }
+
+  /**
+   * @description parse sfc into different block
+   * @returns {object} blocks
+   * @memberof SFCParser
+   */
   parseSFCToBlocks () {
     const { template, script, styles } = parseSFC({
       source: this.source,
       compiler,
       needMap: false
     })
+    if (!template && !script) {
+      throw new Error('At least provide a script block or a template block')
+    }
     this.templateBlock = template
     this.scriptBlock = script
     this.styleBlocks = styles
+
+    return {
+      template,
+      script,
+      styles
+    }
   }
 
+  /**
+   * @description parse script block in sfc, extract import statements, export default object and statements between
+   * @memberof SFCParser
+   */
   parseScriptBlock () {
+    if (!this.scriptBlock) return
     // keep whats left between export default and import
     const componentScriptBlockVisitor = {
       ImportDeclaration: path => {
@@ -75,6 +133,10 @@ class SFCParser {
     babelTraverse(this.scriptBlock.ast, componentScriptBlockVisitor)
   }
 
+  /**
+   * @description verfiy style block, make sure they don are not scoped
+   * @memberof SFCParser
+   */
   verifyStyleBlocks () {
     this.styleBlocks.forEach(style => {
       if (style.module || style.scoped) {
@@ -83,24 +145,30 @@ class SFCParser {
     })
   }
 
+  /**
+   * @description parse template block in sfc into render function
+   * @memberof SFCParser
+   */
   parseTemplateBlock () {
+    if (!this.templateBlock) return
     const templateCompileResult = compileTemplate({
       source: this.templateBlock.content,
       compiler
     })
     this.templateBlock.code = templateCompileResult.code
     this.templateBlock.ast = babelParser.parse(templateCompileResult.code, this.babelParserOptions)
-    this.componentDeclaration = this.componentAstFactory({
-      componentOptions: this.componentOptions,
-      renderFnsAndOtherStatements: [].concat(this.templateBlock.ast.program.body.slice(), this.scriptBlock.ast.program.body.slice()),
-      componentName: babelTypes.identifier(this.componentName)
-    })
   }
 
+  /**
+   * @description parse everything into ast except style tags
+   * @returns {object} sfc asts and infos
+   * @memberof SFCParser
+   */
   parse() {
     this.parseSFCToBlocks()
     this.parseScriptBlock()
     this.parseTemplateBlock()
+    this.genComponentDeclarationAst()
     this.verifyStyleBlocks()
     return {
       componentName: this.componentName,
